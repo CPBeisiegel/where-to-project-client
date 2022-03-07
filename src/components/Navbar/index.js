1d614d94df421b577ca974ed11da0cd51c1bb55c
Danielle Nascimento <danielle_nascimento_@hotmail.com>
2022-03-07 15:23:42 +0100

    fiz navbar and carousel

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,4 +1,7 @@
+import { Link, useNavigate } from "react-router-dom";
+import { useContext, useState, useEffect } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
+import { AuthContext } from "../../contexts/authContext";
 import Logo from "../../assets/images/Viagem.png";
 
 export function NavbarExterna() {
@@ -8,8 +11,8 @@ export function NavbarExterna() {
         <Navbar.Brand>
           <img
             src={Logo}
-            width="30"
-            height="30"
+            width="60"
+            height="60"
             className="d-inline-block align-top"
             alt="WhereTo?"
           />
@@ -17,6 +20,7 @@ export function NavbarExterna() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto navbar-brand ">
+            <Nav.Link to="#home">Home</Nav.Link>
             <Nav.Link to="#home">Estadias</Nav.Link>
             <Nav.Link to="#link">Cadastrar</Nav.Link>
             <Nav.Link to="#link">Entrar</Nav.Link>

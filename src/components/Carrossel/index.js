32233b4ccda8f460cd40df1a30f8a6ddda8260c1
Danielle Nascimento <danielle_nascimento_@hotmail.com>
2022-03-07 14:58:17 +0100

    add logo to navbar

--- src/components/Carrossel/index.js
+++ src/components/Carrossel/index.js
@@ -2,19 +2,14 @@ import { Carousel } from "react-bootstrap";
 
 export function Carrossel() {
   return (
-    <Carousel
-      variant="dark"
-      className="container mt-5 mb-5s"
-      width={100}
-      height={100}
-    >
+    <Carousel>
       <Carousel.Item interval={5000}>
         <img
           className="d-block w-100"
           src="https://a0.muscache.com/im/pictures/bd478462-cfac-4e27-9be2-d5faa1188523.jpg?im_w=960"
           alt="First slide"
         />
-        <Carousel.Caption>
+        <Carousel.Caption style={{ backgroundColor: "#6667AB" }}>
           <h3>Where to... férias luxuosas?</h3>
           <p>
             Perto de Londres, casa costruída em 1710 para aristocracia inglesa
@@ -27,7 +22,7 @@ export function Carrossel() {
           src="https://a0.muscache.com/im/pictures/11bd9fc9-4ca9-4208-b449-bdc63a1969b9.jpg?im_w=960"
           alt="Second slide"
         />
-        <Carousel.Caption>
+        <Carousel.Caption style={{ backgroundColor: "#6667AB" }}>
           <h3>Where to... uma estadia excêntrica?</h3>
           <p>Em Bali, uma casa de bambu única que é uma obra de arte</p>
         </Carousel.Caption>
@@ -38,7 +33,7 @@ export function Carrossel() {
           src="https://a0.muscache.com/im/pictures/a6b1bff2-6660-45f5-89df-37e3edf39bb9.jpg?im_w=960"
           alt="Third slide"
         />
-        <Carousel.Caption>
+        <Carousel.Caption style={{ backgroundColor: "#6667AB" }}>
           <h3>Where to... fim de semana no mar?</h3>
           <p>Em Lisboa, barco remodelado com todo o conforto para um casal. </p>
         </Carousel.Caption>
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,10 +1,19 @@
 import { Navbar, Container, Nav } from "react-bootstrap";
+import Logo from "../../assets/images/Viagem.png";
 
 export function NavbarExterna() {
   return (
     <Navbar collapseOnSelect expand="lg" style={{ backgroundColor: "#6667AB" }}>
       <Container>
-        <Navbar.Brand href="#home">LOGO</Navbar.Brand>
+        <Navbar.Brand>
+          <img
+            src={Logo}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+            alt="WhereTo?"
+          />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto navbar-brand ">

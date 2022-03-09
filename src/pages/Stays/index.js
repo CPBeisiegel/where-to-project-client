81ea7e8c8f7afd67e7bd01489b8b3b4b162a3dd2
Danielle Nascimento <danielle_nascimento_@hotmail.com>
2022-03-09 15:24:12 +0100

    changes in reviews

--- src/pages/Stays/index.js
+++ src/pages/Stays/index.js
@@ -1,5 +1,5 @@
 import { Cards } from "../../components/Card";
-import { SearchBar } from "../../components/SearchBar";
+import { SearchBar } from "../../components/Searchbar";
 import { Container } from "react-bootstrap";
 import "./Stays.css";
 /* import { InternalNavbar } from "../../components/InternalNavbar"; */
